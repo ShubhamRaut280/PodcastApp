686f18989e8c68f44e80e1b316fae220fa09c153
Shubham Raut <124999374+ShubhamRaut280@users.noreply.github.com>
2025-01-06 17:02:18 +0530

    added providing response script

--- src/components/Upload.jsx
+++ src/components/Upload.jsx
@@ -1,19 +1,27 @@
 import React, { useState } from 'react'
 import '../App.css'
 import pdfToText from 'react-pdftotext'
-
+import  generate  from '../Utils/Gemini'
+import { Circles } from 'react-loader-spinner'
 
 const Upload = () => {
     const [file, setFile] = useState(null)
     const [error, setError] = useState('')
     const [uploadTitle, setUploadTitle] = useState('Upload')
+    const [filePath, setFilePath] = useState('')
+    const [loading, setLoading] = useState(false)
+
+    const getScript = async (text) => {
+        const response = await generate(text)
+        setFilePath(response)
+        setLoading(false)
+    }
 
     const handleFileChange = (event) => {
         const selectedFile = event.target.files[0]
         if (selectedFile && selectedFile.type === 'application/pdf') {
             setFile(selectedFile)
             setError('')
-
         } else {
             setFile(null)
             setError('Please select a PDF file')
@@ -22,11 +30,15 @@ const Upload = () => {
 
     const handleUpload = async () => {
         if (file) {
-            setUploadTitle('Uploading...');
-
+            setUploadTitle('Uploading...')
+            setLoading(true)
+            console.log('Uploading...', file)
             pdfToText(file)
-                .then(text => console.log(`final text ${text}`))
-                .catch(error => console.error("Failed to extract text from pdf"))
+                .then(text => getScript(text))
+                .catch(error => {
+                    console.error(`Failed to extract text from pdf ${error}`)
+                    setLoading(false)
+                })
             setUploadTitle('Completed')
         } else {
             console.log('No file selected or invalid file type')
@@ -35,10 +47,13 @@ const Upload = () => {
 
     return (
         <div className="upload-container">
-            <h1 className="upload-title">Upload your pdf</h1>
+            <h1 className="upload-title">{uploadTitle}</h1>
             <input type="file" className="upload-input" onChange={handleFileChange} />
             {error && <p className="error-message">{error}</p>}
-            <button className="upload-button" onClick={handleUpload}>{uploadTitle}</button>
+            <button className="upload-button" onClick={handleUpload} disabled={loading}>
+                {loading ? <Circles height="20" width="20" color="#fff" /> : 'Upload'}
+            </button>
+            {filePath && <a href={filePath} download>Download Generated File</a>}
         </div>
     )
 }
