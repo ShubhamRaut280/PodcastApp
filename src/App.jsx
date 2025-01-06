96b7a515f7b5334aed1f2c41521ca7034ed3292e
Shubham Raut <124999374+ShubhamRaut280@users.noreply.github.com>
2025-01-06 19:00:07 +0530

    enhance layout and styling for app container and title in App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React from 'react'
 import './App.css'
 import Upload from './components/Upload'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div>
-        <Upload/>
+    <div className="app-container">
+      <h2 className="app-title">Automated Podcast Solution</h2>
+      <p className="app-description">Upload a PDF file to generate a podcast script and audio file</p>
+      <Upload />
     </div>
   )
 }
